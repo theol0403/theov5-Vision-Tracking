10c20f9c4ea403fcd29c1c3b07db3936d1362b14
Theo Lemay <theol0403@gmail.com>
2018-05-30 16:44:12 -0700

    Reworked how it returns the vision data

--- src/Driver/DriverVisionTracking.cpp
+++ src/Driver/DriverVisionTracking.cpp
@@ -5,7 +5,16 @@
 #define BALL_SIG 1
 #define BASE_P 1
 
-c::vision_object_s_t visionOutput;
+
+Mutex calculateVisionMutex;
+
+
+c::vision_object_s_t calculateVision()
+{
+  calculateVisionMutex.take(100);
+  return mainVision.get_by_sig(0, BALL_SIG);
+  calculateVisionMutex.give();
+}
 
 
 
@@ -13,10 +22,9 @@ int driverBaseAngle()
 {
   int baseP = BASE_P;
 
-  visionOutput = mainVision.get_by_sig(0, BALL_SIG);
 
-  int x_error = visionOutput.x_middle_coord; // Since 0,0 is the middle of the sensor's feild of veiw (set in initialize), any x deriviation is our error
-
+  int x_error = calculateVision().x_middle_coord;
+  // Since 0,0 is the middle of the sensor's feild of veiw (set in initialize), any x deriviation is our error
 
   int finalBasePower = x_error * baseP;
 
@@ -26,11 +34,20 @@ int driverBaseAngle()
 
 int driverArmAngle()
 {
-  visionOutput = mainVision.get_by_sig(0, BALL_SIG);
 
-  int y_error = visionOutput.y_middle_coord * -1; // Apperantly a positive y is down, I prefer to work with positive y being up
+  int y_error = calculateVision().y_middle_coord * -1; // Apperantly a positive y is down, I prefer to work with positive y being up
 
   int finalArmAngle = y_error; // Eventaully this will be the calculation for an absolute position
 
   return finalArmAngle;
+}
+
+
+
+void displayVision(void*)
+{
+  while(true) {
+    calculateVision();
+  }
+
 }
