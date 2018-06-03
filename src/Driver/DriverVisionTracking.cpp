82ccb7db9f698ca13b58eda10b5d6d4973ccbf0c
Theo Lemay <theol0403@gmail.com>
2018-06-03 14:09:19 -0700

    Added comments and a delay to the vision

--- src/Driver/DriverVisionTracking.cpp
+++ src/Driver/DriverVisionTracking.cpp
@@ -2,44 +2,48 @@
 
 #include "../../Include/Driver/DriverVisionTracking.hpp"
 
-#define BALL_SIG 1
-#define BASE_P 1
+#define BALL_SIG 1 // Defunes the vision signature that is trained for the ball
+#define BASE_P 1 // The Kp for X error / base power
 
 
-Mutex calculateVisionMutex;
+Mutex calculateVisionMutex; // Makes sure no other resource is measuring the vision sensor
+//
 
-
-c::vision_object_s_t calculateVision()
+c::vision_object_s_t calculateVision() //Function to read vision sensor data
 {
-  calculateVisionMutex.take(100);
-  return mainVision.get_by_sig(0, BALL_SIG);
-  calculateVisionMutex.give();
+  calculateVisionMutex.take(100); //Takes mutex so no other instance of function can read at the same time
+  return mainVision.get_by_sig(0, BALL_SIG); // Returns info for largest object of the signature
+  calculateVisionMutex.give(); // Returns mutex
 }
 
 
-
-int driverBaseAngle()
+int driverBaseAngle() //Function that outputs the power to be sent to the base for turning
 {
-  int baseP = BASE_P;
-
+  int baseP = BASE_P; //The P for X error or angle error / motor power
 
   int x_error = calculateVision().x_middle_coord;
   // Since 0,0 is the middle of the sensor's feild of veiw (set in initialize), any x deriviation is our error
+  // If the vision sensor is not centered with the arm, a trig formula needs to be here.
+  // It will then output absolute, or most likely relative angle error. P will have to be changed
 
-  int finalBasePower = x_error * baseP;
+  int finalBasePower = x_error * baseP; // For now a simple P based on X deriviation from the center of the vision
 
-  return finalBasePower;
+  return finalBasePower; //Returns power to be sent to the base
 }
 
 
 int driverArmAngle()
 {
 
-  int y_error = calculateVision().y_middle_coord * -1; // Apperantly a positive y is down, I prefer to work with positive y being up
+  int y_error = calculateVision().y_middle_coord * -1;
+  // Appearently a positive y is down, I prefer to work with positive y being up
+  // Since 0,0 is the middle of the sensor's feild of veiw (set in initialize), any y deriviation is our error
+  // If the vision sensor is not centered with the arm, a trig formula needs to be here.
+  // It will then output absolute angle error, to be P'd by the arm task
 
-  int finalArmAngle = y_error; // Eventaully this will be the calculation for an absolute position
+  int finalArmAngle = y_error; // Eventaully this will be the calculation for an absolute position, but for now it's P
 
-  return finalArmAngle;
+  return finalArmAngle; // Returns final angle the arm needs to be at (currently y error)
 }
 
 
@@ -48,6 +52,7 @@ void displayVision(void*)
 {
   while(true) {
     calculateVision();
+    delay(20);
   }
 
 }
