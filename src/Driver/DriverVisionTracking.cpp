4373483bf9fdaf0b2fc03008c1b3bc6c801c0830
Theo Lemay <theol0403@gmail.com>
2018-06-05 16:45:21 -0700

    found and put in the vision FOV

--- src/Driver/DriverVisionTracking.cpp
+++ src/Driver/DriverVisionTracking.cpp
@@ -9,7 +9,7 @@
 
 float driverBaseAngle() //Function that outputs the power to be sent to the base for turning
 {
-  int x_error = calculateVision().x_middle_coord - VISION_WIDTH/2;
+  int x_error = calculateVision().x_middle_coord - VISION_FOV_WIDTH/2;
   // Centers the vision, and any x deriviation is our error
   // If the vision sensor is not centered with the arm, a trig formula needs to be here.
   // It will then output absolute, or most likely relative angle error. P will have to be changed
@@ -23,7 +23,7 @@ float driverBaseAngle() //Function that outputs the power to be sent to the base
 int driverArmAngle()
 {
 
-  int y_error = (calculateVision().y_middle_coord * -1) + VISION_HEIGHT - VISION_HEIGHT/2;
+  int y_error = (calculateVision().y_middle_coord * -1) + VISION_FOV_HEIGHT - VISION_FOV_HEIGHT/2;
   // Appearently a positive y is down, I prefer to work with positive y being up
   // Centers the vision, and any x deriviation is our error
   // If the vision sensor is not centered with the arm, a trig formula needs to be here.
