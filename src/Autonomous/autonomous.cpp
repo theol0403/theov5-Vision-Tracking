4db2dd0f39495c542815c550e902e51ed214becb
theol0403 <16546293+theol0403@users.noreply.github.com>
2018-06-13 11:59:09 -0700

    it works

--- src/Autonomous/autonomous.cpp
+++ src/Autonomous/autonomous.cpp
@@ -4,24 +4,16 @@
 // comp. control has not been fully implemented yet, so
 // this function will never be called
 
-void baseMotorsMove(int degreesLeft, int degreesRight)
+void baseMotorsMove(int powerLeft, int powerRight, int forwardTime)
 {
   pros::Motor leftBaseMotor(1, pros::c::E_MOTOR_GEARSET_36, false);
   pros::Motor rightBaseMotor(2, pros::c::E_MOTOR_GEARSET_36, true);
 
-  int leftLastPos = leftBaseMotor.get_position();
-  int rightLastPos = rightBaseMotor.get_position();
-  int leftError;
-  int rightError;
-  float driveKp = 0.1;
-  while (true)
-  {
-    leftError = leftBaseMotor.get_position() - degreesLeft - leftLastPos;
-    rightError = rightBaseMotor.get_position() - degreesRight - rightLastPos;
-
-    leftBaseMotor.move(leftError * driveKp);
-    rightBaseMotor.move(rightError * driveKp);
-  }
+  leftBaseMotor.move(powerLeft);
+  rightBaseMotor.move(powerRight);
+  delay(forwardTime);
+  leftBaseMotor.move(0);
+  rightBaseMotor.move(0);
 }
 
 
@@ -29,8 +21,11 @@ void baseMotorsMove(int degreesLeft, int degreesRight)
 void autonomous()
 {
 
-  baseMotorsMove(700, 700);
-  delay(4000);
-  baseMotorsMove(700, 700);
-
+  baseMotorsMove(127, 127, 2000);
+  delay(1000);
+  baseMotorsMove(-127, -127, 2000);
+  delay(1000);
+  baseMotorsMove(127, -127, 1000);
+  delay(1000);
+  baseMotorsMove(-127, 127, 1000);
 }
