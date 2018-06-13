4c0b9ff3a5d379e116159ebba371bbdf126a041c
theol0403 <16546293+theol0403@users.noreply.github.com>
2018-06-13 11:47:23 -0700

    not working P loop

--- src/Autonomous/autonomous.cpp
+++ src/Autonomous/autonomous.cpp
@@ -6,10 +6,22 @@
 
 void baseMotorsMove(int degreesLeft, int degreesRight)
 {
-	pros::Motor leftBaseMotor(1, pros::c::E_MOTOR_GEARSET_36, false);
+  pros::Motor leftBaseMotor(1, pros::c::E_MOTOR_GEARSET_36, false);
   pros::Motor rightBaseMotor(2, pros::c::E_MOTOR_GEARSET_36, true);
-	leftBaseMotor.move_relative(degreesLeft, 127);
-	rightBaseMotor.move_relative(degreesRight, 127);
+
+  int leftLastPos = leftBaseMotor.get_position();
+  int rightLastPos = rightBaseMotor.get_position();
+  int leftError;
+  int rightError;
+  float driveKp = 0.1;
+  while (true)
+  {
+    leftError = leftBaseMotor.get_position() - degreesLeft - leftLastPos;
+    rightError = rightBaseMotor.get_position() - degreesRight - rightLastPos;
+
+    leftBaseMotor.move(leftError * driveKp);
+    rightBaseMotor.move(rightError * driveKp);
+  }
 }
 
 
@@ -17,7 +29,8 @@ void baseMotorsMove(int degreesLeft, int degreesRight)
 void autonomous()
 {
 
-baseMotorsMove(700, 700);
-baseMotorsMove(-700, -700);
+  baseMotorsMove(700, 700);
+  delay(4000);
+  baseMotorsMove(700, 700);
 
 }
